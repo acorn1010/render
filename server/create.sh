2fcb36b488d83e18e13b878dfcd08389052d0fbd
acorn1010 <github@unallied.com>
2023-01-01 13:53:46 -0800

    Update create.sh script

--- server/create.sh
+++ server/create.sh
@@ -6,7 +6,7 @@ set -e
 
 # Prerequisites
 # sudo apt-get install libssh2-1-dev libevent-dev
-# wget https://github.com/vitobotta/hetzner-k3s/releases/download/v0.6.5/hetzner-k3s-linux-x86_64
+# wget https://github.com/vitobotta/hetzner-k3s/releases/download/v0.6.7/hetzner-k3s-linux-x86_64
 # chmod +x hetzner-k3s-linux-x86_64
 # sudo mv hetzner-k3s-linux-x86_64 /usr/local/bin/hetzner-k3s
 
@@ -15,3 +15,28 @@ echo "Creating cluster."
 . .env
 export HETZNER_TOKEN="${HETZNER_TOKEN}"
 hetzner-k3s create --config <(envsubst < ./fleet.yaml)
+
+# Once-off. If ran again, these are no-op
+helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+helm repo add jetstack https://charts.jetstack.io
+helm repo add traefik https://traefik.github.io/charts
+# Update Helm chart repository cache
+helm repo update
+
+# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.crds.yaml
+helm install cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.10.1 \
+  --set installCRDs=true
+
+helm install rancher rancher-stable/rancher \
+  --namespace cattle-system \
+  --create-namespace \
+  --set hostname=rancher.acorn1010.com \
+  --set ingress.tls.source=letsEncrypt \
+  --set letsEncrypt.email=admin@foony.com \
+  --set letsEncrypt.ingress.class=traefik
+
+# Add traefik so we can access Rancher externally.
+helm install traefik traefik/traefik --namespace traefik --create-namespace
