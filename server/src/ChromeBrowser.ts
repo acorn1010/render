fff6ee03b1b4ad0ae1179084b3343be1344273a6
acorn1010 <github@unallied.com>
2023-01-23 13:54:32 -0800

    Fix broken response when exceeding network timeout

--- server/src/ChromeBrowser.ts
+++ server/src/ChromeBrowser.ts
@@ -22,22 +22,19 @@ export async function render(url: string, requestHeaders: Record<string, string>
     browser.createIncognitoBrowserContext(),
     browser.userAgent(),
   ]);
-  console.log('Original user agent', originalUserAgent);
   try {
     const page = await context.newPage();
     try {
       page.setDefaultTimeout(20_000);
 
       // TODO(acorn1010): Try to close JavaScript dialogs after 1 second
-      // TODO(acorn1010): Add support for continuing on certificate errors
-
-      // await Promise.all([
-      //     page.setUserAgent(`${originalUserAgent} Acorn1010 (+https://https://github.com/acorn1010/render)`),
-      //     page.setExtraHTTPHeaders(requestHeaders),
-      // ]);
 
       const response = await page.goto(url);
-      const html = await page.content(); // await page.evaluate(() => document.);
+      let html = await page.content();
+      try {
+        await page.waitForNetworkIdle({timeout: 3_000});
+        html = await page.content();
+      } catch (e: any) {}  // Ignore timeouts when the network isn't idle
       const responseHeaders = response?.headers() ?? {};
       const statusCode = response?.status() ?? 400;
       if (response && !responseHeaders['content-type']?.includes('text/html')) {
