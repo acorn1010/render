ca316afeb8a5a269acac0b086fe1b6b6d746a0c0
acorn1010 <github@unallied.com>
2023-02-20 08:52:30 -0800

    Add Brotli compression

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -3,12 +3,17 @@ import {render} from "../browsers/ChromeBrowser";
 import {env} from "../Environment";
 import {RenderResponse} from "../db/Schema";
 import * as Url from "url";
+import * as zlib from "zlib";
+import { promisify } from "util";
 
 // Don't include some problematic headers from the original third-party response. We're using our
 // own content-encoding, we don't support keep-alive connections, and we don't do chunked encoding.
 const HEADER_BLACK_LIST = new Set(['transfer-encoding', 'connection', 'content-encoding']);
 
 const CACHE_TIME_MS = 24 * 60 * 60 * 1_000;
+
+const decompressBrotli = promisify(zlib.brotliDecompress);
+const compressBrotli = promisify(zlib.brotliCompress);
 
 export async function doRequest(req: express.Request, res: express.Response) {
   const start = Date.now();
@@ -44,7 +49,11 @@ export async function doRequest(req: express.Request, res: express.Response) {
           .pexpire(`{users:${userId}}:fetches:${yyyyMm}`, 365 * 24 * 60 * 60 * 1_000/*, 'NX'*/)  // NOTE: 'NX' is supported as of v7. Update as soon as it's stable
           .exec()) as any as [[null, RenderResponse], [null, Buffer]];
   if (metadata?.[1] && data?.[1]) {
-    response = {...JSON.parse(metadata[1] as any), buffer: data[1]};
+    response = {
+      ...JSON.parse(metadata[1] as any),
+      // TODO(acorn1010): Remove when everything is Brotli'd
+      buffer: await decompressBrotli(data[1]).catch(() => data[1]),
+    };
   }
 
   // TODO(acorn1010): Instead of trying to render this directly, stick it in a worker queue
@@ -55,17 +64,20 @@ export async function doRequest(req: express.Request, res: express.Response) {
       const {buffer, ...rest} = response;
       // Set the page cache and increment the number of pages this user has cached this month.
       const now = Date.now();
-      const commander = env.redis.multi()
-          .incr(`{users:${userId}}:renderCounts:${yyyyMm}`)  // Number of times user has rendered a page
-          .pexpire(`{users:${userId}}:renderCounts:${yyyyMm}`, 365 * 24 * 60 * 60 * 1_000/*, 'NX'*/)
-          .set(`${key}:m`, JSON.stringify({...rest, renderTimeMs: now - start}), 'PX', CACHE_TIME_MS)
-          .setBuffer(`${key}:d`, Buffer.from(buffer), 'PX' as any, CACHE_TIME_MS as any);
-      // If this request succeeded, then log when it expires so we can refresh it before cache
-      // expiration.
-      if (response.statusCode < 400) {
-        commander.zadd(`{users:${userId}}:expiresAt`, now + CACHE_TIME_MS, url)
-      }
-      commander.exec().then(() => {});
+      const statusCode = response.statusCode;
+      compressBrotli(Buffer.from(buffer)).then(compressed => {
+        const commander = env.redis.multi()
+            .incr(`{users:${userId}}:renderCounts:${yyyyMm}`)  // Number of times user has rendered a page
+            .pexpire(`{users:${userId}}:renderCounts:${yyyyMm}`, 365 * 24 * 60 * 60 * 1_000/*, 'NX'*/)
+            .set(`${key}:m`, JSON.stringify({...rest, renderTimeMs: now - start}), 'PX', CACHE_TIME_MS)
+            .setBuffer(`${key}:d`, compressed, 'PX' as any, CACHE_TIME_MS as any);
+        // If this request succeeded, then log when it expires so we can refresh it before cache
+        // expiration.
+        if (statusCode < 400) {
+          commander.zadd(`{users:${userId}}:expiresAt`, now + CACHE_TIME_MS, url);
+        }
+        commander.exec();
+      });
     }
   }
 
