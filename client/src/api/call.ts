675483cd50eca051abe27baac3681cd50197d368
acorn1010 <github@unallied.com>
2023-04-09 15:16:47 -0700

    Add support for client optimism in long poll
    Add support for optimistic clients with long polling. This lets us update the state immediately after an API call so we can show the latest value to the client even before the server is done.

--- client/src/api/call.ts
+++ client/src/api/call.ts
@@ -1,40 +1,83 @@
 import {Actions} from 'render-shared-library/lib/Action';
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {authStore} from "@/auth/authStore";
+import {createGlobalStore} from "@/state/createGlobalStore";
 
 type CallArgs<T extends keyof Actions> = Actions[T]['input'] extends any[]
     ? Actions[T]['input']
     : {} extends Actions[T]['input'] ? [] : [Actions[T]['input']];
 
 /**
- * Periodically polls `action` with the given `args`. Returns `undefined` until the API has had a
- * chance to reply.
- *
- * TODO(acorn1010): Add Multiset so we can support batching in case multiple components need access
- *  to the same endpoint.
+ * Interval in milliseconds of long-polling. Determines how frequently data is polled from the
+ * server, where lower values mean more frequent polling.
  */
-export function useLongPoll<T extends keyof Actions>(action: T, ...args: CallArgs<T>):
-    Actions[T]['output'] | undefined {
-  const [result, setResult] = useState<Actions[T]['output'] | undefined>(undefined);
+const LONG_POLL_INTERVAL_MS = 10_000;
 
-  useEffect(() => {
-    const refresh = async () => {
-      try {
-        setResult(await call[action](...args));
-      } catch (e) {
-        console.error(`Failed to do long poll for ${action} with args: ${args}`, e);
+const pollStore = createGlobalStore({} as {[key: string]: any});
+
+/**
+ * Allows periodic polling of state from the server. This is essentially a "slow" pub/sub without
+ * SSE or websockets.
+ */
+export const poll = {
+  /**
+   * Updates the long-poll data for `action` with the given `args`. This is a temporary update.
+   * After a timeout greater than `LONG_POLL_INTERVAL_MS` (or during the next long-poll), this
+   * update will be cleared and replaced with the long-poll data.
+   */
+  update: <T extends keyof Actions>(action: T, ...args: CallArgs<T>) => (data: Partial<Actions[T]['output']>) => {
+    const key = JSON.stringify([action, ...args]);
+    const scope: {ref: Partial<Actions[T]['output']>} = {ref: {}};
+    pollStore.set(key, prevState => {
+      scope.ref = ({...prevState, ...data});
+      return scope.ref;
+    });
+    setTimeout(() => {
+      if (scope.ref === pollStore.get(key)) {
+        // State is stale and hasn't changed. Delete it!
+        pollStore.delete(key);
       }
-    };
-    const timer = setInterval(refresh, 10_000);
-    refresh().then(() => {});
+    }, LONG_POLL_INTERVAL_MS * 1.5);
+  },
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, [action, ...args]);
+  /**
+   * Periodically polls `action` with the given `args`. Returns `undefined` until the API has had a
+   * chance to reply.
+   *
+   * TODO(acorn1010): Add Multiset so we can support batching in case multiple components need access
+   *  to the same endpoint.
+   */
+  use: <T extends keyof Actions>(action: T, ...args: CallArgs<T>): Actions[T]['output'] | undefined => {
+    const key = JSON.stringify([action, ...args]);
+    const [result, setResult] = pollStore.use(key);
 
-  return result;
-}
+    useEffect(() => {
+      const refresh = async () => {
+        try {
+          // NOTE: It's possible that we've updated the client's state in the time we've started
+          // fetching from the server. If so, don't update the local state.
+          const oldState = pollStore.get(key);
+          const result = await call[action](...args);
+          if (oldState === pollStore.get(key)) {
+            setResult(result);  // State is still the same. It's safe to replace!
+          }
+        } catch (e) {
+          console.error(`Failed to do long poll for ${action} with args: ${args}`, e);
+        }
+      };
+      const timer = setInterval(refresh, LONG_POLL_INTERVAL_MS);
+      refresh().then(() => {});
+
+      return () => {
+        clearInterval(timer);
+      };
+    }, [action, ...args]);
+
+    return result;
+  },
+};
+
+poll.update('getProfile');
 
 /**
  * Calls the server API. The call will be directed to the server for a room if the player is in a
@@ -49,7 +92,7 @@ export const call: {[K in keyof Actions]: (...args: CallArgs<K>) => Promise<Acti
 
 async function _call<T extends keyof Actions>(
     action: T, ...args: CallArgs<T>): Promise<Actions[T]['output']> {
-  const url = import.meta.env.DEV ? 'http://localhost:3000/api' : 'https://seorender.site/api';
+  const url = import.meta.env.DEV ? 'http://localhost:3000/api' : 'https://render.acorn1010.com/api';
   authToken = await queryAuthToken();
   const response = await fetch(url, {
     method: 'POST',
